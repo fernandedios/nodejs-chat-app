dac739875de0fb654a653592e76cf04edb23e48e
Fernan de Dios <info@mediagramstudios.com>
2017-09-16 14:42:12 +0800

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -31,12 +31,14 @@ socket.on('newLocationMessage', function(message) {
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
 
-  }, function(data) { // pass callback method
-
+  }, function() { // pass callback method
+    messageTextbox.val(''); // clear value
   });
 });
 
@@ -47,13 +49,17 @@ locationButton.on('click', function(e) {
     return alert('Geolocation not supported');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   // get user location
   navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.removeAttr('disabled').text('Send Location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function() {
+    locationButton.removeAttr('disabled').text('Send Location');
     alert('Unable to fetch location');
   })
 });
