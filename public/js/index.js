d0b719077d381f59ce4500f5451deaa04bdb6644
Fernan de Dios <info@mediagramstudios.com>
2017-09-18 10:21:20 +0800

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -14,21 +14,28 @@ socket.on('newMessage', function(message) {
   // console.log('newMessage', message);
   var formattedTime = moment(message.createdAt).format('h:mm a');
 
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  jQuery('#messages').append(li);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message) {
+  // console.log('newLocationMessage', message);
   var formattedTime = moment(message.createdAt).format('h:mm a');
 
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
-  a.attr('href', message.url);
-  li.text(`${message.from} ${formattedTime}: `);
-  li.append(a);
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    url: message.url,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function(e) {
